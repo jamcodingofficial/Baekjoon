c2b7a96bf3a70f46958d089b01cb52a109b65c7d
JC <116627493+jamcodingofficial@users.noreply.github.com>
2022-10-26 15:06:38 +0900

    Create 24051.cpp

---
+++ C++/정렬알고리즘 - 초급/24051.cpp
@@ -0,0 +1,39 @@
+/*
+ * Author : JC
+ * https://www.acmicpc.net/problem/24051
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int arr[10000];
+int n, k, cnt;
+
+int main(){
+  
+    cin >> n >> k;
+    
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+   
+    for(int i=n-1; i>=0; i--){
+        for(int j=0; j<i; j++){
+            if(arr[j] > arr[j+1]){
+                int tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+                cnt++;
+                if(cnt==k){
+                    cout << arr[j] << " " << arr[j+1];
+                    return 0;
+                }
+            }
+        }
+    }
+    
+    cout << -1;
+    
+    return 0;
+}
