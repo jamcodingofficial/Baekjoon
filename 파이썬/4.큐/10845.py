d79b74bd49e00f1184fd326160ead42403c42837
jamcodingofficial <jhcho@jamcoding.co.kr>
2022-10-29 18:23:53 +0900

    .

---
+++ 파이썬/4.큐/10845.py
@@ -0,0 +1,37 @@
+'''
+ * Author : JC
+ * https://www.acmicpc.net/problem/10845
+'''
+
+import sys
+input = sys.stdin.readline # 입력 속도를 빠르게 만들기 위해 꼭 쓴다.
+
+n = int(input())
+queue = []
+for i in range(n) :
+    a = input().split()
+    if a[0] == 'push' : 
+        queue.append(a[1])
+    elif a[0] == 'pop' : 
+        if len(queue) == 0 :
+            print(-1)
+        else :
+            print(queue[-1])
+            queue.pop(0)
+    elif a[0] == 'size' : 
+        print(len(queue))
+    elif a[0] == 'empty' :
+        if len(queue) == 0 :
+            print(1)
+        else :
+            print(0)
+    elif a[0] == 'front' :
+        if len(queue) == 0 :
+            print(-1)
+        else :
+            print(queue[0])
+    else :
+        if len(queue) == 0 :
+            print(-1) 
+        else :
+            print(queue[-1])
