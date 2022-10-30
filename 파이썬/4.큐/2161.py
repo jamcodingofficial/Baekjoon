2ddb7d1c42e220d588b9572072755a052d916234
jamcodingofficial <jhcho@jamcoding.co.kr>
2022-10-30 11:58:59 +0900

    .

--- 파이썬/4.큐/2161.py
+++ 파이썬/4.큐/2161.py
@@ -5,10 +5,7 @@
 
 n = int(input())
 
-# l = [i for i in range(1, n+1)] -> [Line 9 ~ 11] 코드를 이렇게 한 줄로 작성해도 됨.
-l = []
-for i in range(1, n+1) :
-    l.append(i)
+l = [i for i in range(1, n+1)]
 
 while len(l) > 1 :
     print(l[0], end = ' ')
