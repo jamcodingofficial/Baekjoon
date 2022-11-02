c19616298d0b4ba7ac20c883ae4b0c7317a0e724
jamcodingofficial <jhcho@jamcoding.co.kr>
2022-11-02 12:10:39 +0900

    .

---
+++ C++/정렬알고리즘 - 초급/2750.cpp
@@ -0,0 +1,26 @@
+/*
+ * Author : JC
+ * https://www.acmicpc.net/problem/2750
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int arr[1000];
+int n;
+
+int main(){
+  
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    // 5 2 3 4 1
+
+    for(int i=0; i<n-1; i++){
+        
+    }
+    
+    return 0;
+}
