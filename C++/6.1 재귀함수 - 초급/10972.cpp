57609d324163c6047b1b760b8ca138745c095dd4
jamcodingofficial <jhcho@jamcoding.co.kr>
2023-02-12 18:48:32 +0900

    .

---
+++ C++/6.1 재귀함수 - 초급/10972.cpp
@@ -0,0 +1,21 @@
+/*
+ * Author : JC
+ * https://www.acmicpc.net/problem/10872
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int factorial(int x){
+    if(x == 0 || x == 1){
+        return 1;
+    }
+    return x * factorial(x-1);
+}
+int main(){
+    cin >> n;
+    cout << factorial(n);
+}
