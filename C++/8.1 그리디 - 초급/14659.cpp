7732c054fa926fae9976a94d2f117fd8a3cfda38
jamcodingofficial <jhcho@jamcoding.co.kr>
2023-02-25 18:34:35 +0900

    .

---
+++ C++/8.1 그리디 - 초급/14659.cpp
@@ -0,0 +1,39 @@
+
+/*
+ * Author : JC
+ * https://www.acmicpc.net/problem/5585
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, res;
+int arr[30000];
+
+int main(){
+    
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    
+    for(int i=0; i<n-1; i++){
+        int cnt = 0;
+        for(int j=i+1; j<n; j++){
+            if(arr[i] > arr[j]){
+                cnt++;
+            }else{
+                break;
+            }
+        }
+        res = max(res, cnt);
+    }
+    
+    cout << res;
+    
+    return 0;
+}
