1584d51323370ad470ec4f4331107ab08b18303c
jamcodingofficial <jhcho@jamcoding.co.kr>
2023-03-07 15:16:54 +0900

    .

---
+++ C++/8.3 그리디 - 고급/23559.cpp
@@ -0,0 +1,34 @@
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, x, a, b;
+int sum;
+vector<int> v;
+
+int main(){
+
+    cin >> n >> x;
+    for(int i=0; i<n; i++){
+        cin >> a >> b;
+        v.push_back(a-b);
+        sum += b;
+    }
+    x = x - (1000 * n);
+    sort(v.rbegin(), v.rend());
+
+    for(int i=0; i<n; i++){
+        if(x >= 4000 && v[i] > 0){
+            x -= 4000;
+            sum += v[i];
+        }
+    }
+
+    cout << sum;
+    
+    return 0;
+}
