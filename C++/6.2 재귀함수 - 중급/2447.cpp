eb9e6044bed4a94814a5aed98961712b4f07f233
jamcodingofficial <jhcho@jamcoding.co.kr>
2023-03-07 15:15:23 +0900

    .

--- C++/6.2 재귀함수 - 중급/2447.cpp
+++ C++/6.2 재귀함수 - 중급/2447.cpp
@@ -1,7 +1,37 @@
 /*
  * Author : JC
- * https://www.acmicpc.net/problem/2447
+ * https://www.acmicpc.net/problem/23559
 */
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
 
+int n, x, a, b;
+int sum;
+vector<int> v;
+
+int main(){
+
+    cin >> n >> x;
+    for(int i=0; i<n; i++){
+        cin >> a >> b;
+        v.push_back(a-b);
+        sum += b;
+    }
+    x = x - (1000 * n);
+    sort(v.rbegin(), v.rend());
+    
+    for(int i=0; i<n; i++){
+        if(x >= 4000 && v[i] > 0){
+            x -= 4000;
+            sum += v[i];
+        }
+    }
+
+    cout << sum;
+    
+    return 0;
+}
