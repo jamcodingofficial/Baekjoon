e3cdf8284eb4a5df9c62a0ffca05dd0c7e6d9774
jamcodingofficial <jhcho@jamcoding.co.kr>
2023-04-23 14:38:17 +0900

    .

--- C++/27. 이진 검색 알고리즘 - 초급/1764.cpp
+++ C++/27. 이진 검색 알고리즘 - 초급/1764.cpp
@@ -3,6 +3,11 @@
  * https://www.acmicpc.net/problem/1764
 */
 
+/*
+ * Author : JC
+ * https://www.acmicpc.net/problem/1764
+*/
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -10,30 +15,47 @@
 using namespace std;
 
 int n, m;
-vector <string> v, r;
+string A[500000], B[500000];
+
+int binarySearch(int len, string target){
+    int low = 0;
+    int high = len-1;
+    int mid;
+    while(low <= high){
+        mid = (low + high) / 2;
+        if(A[mid] == target){
+            return mid;
+        }else if(A[mid] < target){
+            low = mid+1;
+        }else{
+            high = mid-1;
+        }
+    }
+    return -1;
+}
 
 int main(){
     
     cin >> n >> m;
-
-    for(int i=0; i<n+m; i++){
-        string s;
-        cin >> s;
-        v.push_back(s);
+    for(int i=0; i<n; i++){
+        cin >> A[i];
     }
-    int cnt = 0;
-    sort(v.begin(), v.end());
-    for(int i=0; i<n+m-1; i++){
-        if(v[i] == v[i+1]){
-            cnt++;
-            r.push_back(v[i]);
+    for(int i=0; i<m; i++){
+        cin >> B[i];
+    }
+    sort(A, A+n);
+    vector<string> v;
+    for(int i=0; i<m; i++){
+        if(binarySearch(n, B[i]) != -1){
+            v.push_back(B[i]);
         }
     }
 
-    cout << cnt << "\n";
-    for(auto elem : r){
-        cout << elem << "\n";
+    sort(v.begin(), v.end());
+    cout << v.size() << "\n";
+    for(int i=0; i<v.size(); i++){
+        cout << v[i] << "\n";
     }
+    
     return 0;
 }
-
