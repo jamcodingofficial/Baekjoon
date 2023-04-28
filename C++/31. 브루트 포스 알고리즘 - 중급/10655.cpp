cb8aace1e75804cdba3658959906259b632de346
jamcodingofficial <jhcho@jamcoding.co.kr>
2023-04-28 11:41:49 +0900

    .

--- C++/31. 브루트 포스 알고리즘 - 중급/10655.cpp
+++ C++/31. 브루트 포스 알고리즘 - 중급/10655.cpp
@@ -5,42 +5,42 @@
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
+typedef pair<int,int> pii;
 
-vector<pair<int, int>> vp;
-int n, sx, sy, x, y;
-int sum;
+vector<pii> vp;
 
-int dist(int sx, int x, int sy, int y){
-    return abs(sx - x) + abs(sy - y);
+int dist(int x1, int x2, int y1, int y2){
+    return abs(x1-x2) + abs(y1-y2);
 }
 
 int main() {
-    
-    cin >> n >> sx >> sy;
-    vp.push_back({sx, sy});
-    
+    int n, nx, ny;
+    cin >> n >> nx >> ny;
+    vp.push_back({nx, ny});
+    int sum = 0;
     for(int i=0; i<n-1; i++){
+        int x, y;
         cin >> x >> y;
         vp.push_back({x, y});
-        sum += dist(sx, x, sy, y);
-        sx = x;
-        sy = y;
+        sum += dist(nx, x, ny, y);
+        nx = x;
+        ny = y;
     }
-    pair<int, int> a, b, c;
+    
+    pii a, b, c;
     int ans = 0;
     for(int i=1; i<n-1; i++){
         a = vp[i-1];
         b = vp[i];
         c = vp[i+1];
-        int d = dist(a.first, b.first, a.second, b.second) + dist(b.first, c.first, b.second, c.second);
-        int e = dist(a.first, c.first, a.second, c.second);
-        int f = d - e;
-        ans = max(ans, f);
+        int r1 = dist(a.first, b.first, a.second, b.second) + dist(b.first, c.first, b.second, c.second);
+        int r2 = dist(a.first, c.first, a.second, c.second);
+        int r3 = r1 - r2;
+        ans = max(ans, r3);
     }
     cout << sum - ans;
-    
+
     return 0;
 }
