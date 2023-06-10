93945f28f74c345274948ecf0cb8a7e849c1b1f7
jamcodingofficial <jhcho@jamcoding.co.kr>
2023-06-10 10:01:08 +0900

    .

--- C++/39. 그래프-트리/1260.cpp
+++ C++/39. 그래프-트리/1260.cpp
@@ -3,6 +3,7 @@
  * https://www.acmicpc.net/problem/1260
 */
 
+// 인접행렬 구현
 #include <iostream>
 #include <string.h>
 #include <queue>
@@ -55,4 +56,69 @@ int main(){
     bfs(v);
 
     return 0;
+}
+
+
+// 인접 리스트 구현
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <string.h>
+
+using namespace std;
+
+vector<int> vec[1001];
+bool visited[1001];
+
+void dfs(int node){
+    cout << node << " ";
+    visited[node] = true;
+    for(int i=0; i<vec[node].size(); i++){
+        if(!visited[vec[node][i]]){
+            dfs(vec[node][i]);
+        }
+    }
+}
+
+
+queue<int> q;
+void bfs(int node){
+    cout << node << " ";
+    visited[node] = true;
+    q.push(node);
+    while(!q.empty()){
+        int p = q.front();
+        q.pop();
+        for(int i=0; i<vec[p].size(); i++){
+            if(!visited[vec[p][i]]){
+                q.push(vec[p][i]);
+                cout << vec[p][i] << " ";
+                visited[vec[p][i]] = true;
+            }
+        }
+    }
+}
+
+int main(){
+    int n, m, v;
+    cin >> n >> m >> v;
+    for(int i=0; i<m; i++){
+        int x, y;
+        cin >> x >> y;
+        vec[x].push_back(y);
+        vec[y].push_back(x);
+    }
+
+    
+    for(int i=1; i<=n; i++){
+        if(!vec[i].empty()){
+            sort(vec[i].begin(), vec[i].end());
+        }
+    }
+    
+    dfs(v);
+    cout << "\n";
+    memset(visited, false, sizeof(visited));
+    bfs(v);
 }
