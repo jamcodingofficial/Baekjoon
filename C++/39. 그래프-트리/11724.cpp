33afc6d0b7852f09694ad472aba03f517a6cec5a
jamcodingofficial <jhcho@jamcoding.co.kr>
2023-06-11 17:19:54 +0900

    .

--- C++/39. 그래프-트리/11724.cpp
+++ C++/39. 그래프-트리/11724.cpp
@@ -3,6 +3,7 @@
  * https://www.acmicpc.net/problem/11724
 */
 
+// 인접행렬 구현
 #include <iostream>
 
 using namespace std;
@@ -38,5 +39,49 @@ int main(){
 
     cout << c;
 
+    return 0;
+}
+
+// 인접 리스트 구현
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> vec[1001];
+bool visited[1001];
+int n, m, cnt;
+
+void dfs(int node){
+    visited[node] = true;
+    for(int i=0; i<vec[node].size(); i++){
+        if(!visited[vec[node][i]]){
+            dfs(vec[node][i]);
+        }
+    }
+}
+
+int main(){
+    cin >> n >> m;
+    for(int i=0; i<m; i++){
+        int u, v;
+        cin >> u >> v;
+        vec[u].push_back(v);
+        vec[v].push_back(u);
+    }
+    for(int i=1; i<=n; i++){
+        sort(vec[i].begin(), vec[i].end());
+    }
+
+    for(int i=1; i<=n; i++){
+        if(!visited[i]){
+            dfs(i);
+            cnt++;
+        }
+    }
+    
+    cout << cnt;
+            
     return 0;
 }
