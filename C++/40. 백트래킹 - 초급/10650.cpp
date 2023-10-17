b0ebd9188a0f5aedb4abe3108a0cde658569e759
jamcodingofficial <jhcho@jamcoding.co.kr>
2023-10-17 14:25:11 +0900

    .

--- C++/40. 백트래킹 - 초급/10650.cpp
+++ C++/40. 백트래킹 - 초급/10650.cpp
@@ -1,28 +1,28 @@
 /*
  * Author : JC
- * https://www.acmicpc.net/problem/15649
+ * https://www.acmicpc.net/problem/15650
 */
 
 #include <iostream>
 
 using namespace std;
 
-int arr[9];
 bool visited[9];
+int arr[9];
 int n, m;
 
-void dfs(int x){ 
+void dfs(int x, int y){
     if(x == m){
         for(int i=0; i<m; i++){
             cout << arr[i] << " ";
         }
         cout << "\n";
     }else{
-        for(int i=1; i<=n; i++){
+        for(int i=y+1; i<=n; i++){
             if(!visited[i]){
                 visited[i] = true;
                 arr[x] = i;
-                dfs(x+1);
+                dfs(x+1, i);
                 visited[i] = false;
             }
         }
@@ -30,8 +30,8 @@ void dfs(int x){
 }
 
 int main(){
-    cin >> n >> m; 
-    dfs(0);
-    
+    cin >> n >> m;
+    dfs(0, 0);
+
     return 0;
 }
