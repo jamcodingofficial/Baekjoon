53aa27535390c658bebdf911a5d884cea3eb6a78
jamcodingofficial <jhcho@jamcoding.co.kr>
2023-12-15 14:48:12 +0900

    .

--- C++/46. 투 포인터 - 초급/2018.cpp
+++ C++/46. 투 포인터 - 초급/2018.cpp
@@ -2,3 +2,31 @@
  * Author : JC
  * https://www.acmicpc.net/problem/2018
 */
+
+#include <iostream>
+
+using namespace std;
+
+int n, sum, cnt;
+
+int main(){
+    int n;
+    cin >> n;
+    int left = 1;
+    int right = 1;
+    
+    while(left <= n){
+        if(sum < n){
+            sum += right++;
+        }else if(sum >= n){
+            if(sum == n){
+                cnt++;
+            }
+            sum -= left++;
+        }
+    }
+    
+    cout << cnt;
+
+    return 0;
+}
