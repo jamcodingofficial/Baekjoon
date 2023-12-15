ab94dd3ea7a4f094840a6a166f330d7066ee2332
jamcodingofficial <jhcho@jamcoding.co.kr>
2023-12-15 12:05:13 +0900

    .

--- C++/46. 투 포인터 - 초급/2003.cpp
+++ C++/46. 투 포인터 - 초급/2003.cpp
@@ -2,3 +2,37 @@
  * Author : JC
  * https://www.acmicpc.net/problem/2003
 */
+
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int arr[10000];
+int l, r;
+int sum, cnt;
+
+int main(){
+
+    cin >> n >> m;
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    
+    while(r <= n){
+        if(sum < m){
+            sum = sum + arr[r];
+            r++;
+        }else if(sum >= m){
+            if(sum == m){
+                cnt++;
+            }
+            sum = sum - arr[l];
+            l++;
+        }
+    }
+    
+    cout << cnt;
+
+    return 0;   
+}
